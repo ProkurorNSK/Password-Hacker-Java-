1d9aede20aa23b7a250f35fea46c958bc9b782dc
ProkurorNSK <fedyashin.pavel@yandex.ru>
2024-03-29 18:51:28 +0700

    2

--- Password Hacker (Java)/task/src/hacker/Main.java
+++ Password Hacker (Java)/task/src/hacker/Main.java
@@ -4,6 +4,8 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,11 +13,49 @@ public class Main {
              DataInputStream input = new DataInputStream(socket.getInputStream());
              DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
 
-            output.writeUTF(args[2]);
-            String receivedMsg = input.readUTF();
-            System.out.println(receivedMsg);
+        String receivedMsg;
+        char[] pass = new char[1];
+        pass[0] = getFirstToken();
+
+        while (true) {
+            output.writeUTF(new String(pass));
+            receivedMsg = input.readUTF();
+            if (Objects.equals(receivedMsg, "Connection success!")) {
+                System.out.println(pass);
+                break;
+            }
+
+            for (int i = pass.length - 1; i >= 0; i--) {
+                char temp = pass[i];
+                pass[i] = getNextToken(pass[i]);
+                if (!isLastToken(temp)) {
+                    break;
+                } else if (i == 0) {
+                    pass = Arrays.copyOf(pass, pass.length + 1);
+                    pass[pass.length - 1] = getFirstToken();
+                }
+            }
+        }
+
 
         } catch (IOException ignored) {
         }
+    }
+
+    static boolean isLastToken(char token) {
+        return token == '9';
+    }
+
+    static char getFirstToken() {
+        return 'a';
+    }
+
+    static char getNextToken(char token) {
+        switch (token) {
+            case 'z' -> token = '0';
+            case '9' -> token = 'a';
+            default -> token++;
+        }
+        return token;
     }
 }
